bf852d3609bddb045dffc37b64503e685fe40153
Erik Karsten <erik@opcode.xyz>
2024-07-24 18:12:13 +0300

    fix: header on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 import logo from "../public/title.png"
+import small from "../public/logo.png"
 
 export function Header() {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
-      <Link href="/"><Image src={logo} alt="Cortex" width={200} height={100} /></Link>
+      <Link className="hidden lg:block" href="/"><Image src={logo} alt="Cortex" width={200} height={100} /></Link>
+      <Link className="lg:hidden" href="/"><Image src={small} alt="Cortex" width={40} height={40} /></Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
         <Link
           href="/guide"
