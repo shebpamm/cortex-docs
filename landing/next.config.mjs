a8dbfb43af8408a38b71b1c9ce0975130120b550
Erik Karsten <erik@opcode.xyz>
2024-07-25 10:51:28 +0300

    fix: use basepath

--- landing/next.config.mjs
+++ landing/next.config.mjs
@@ -7,11 +7,11 @@ const nextConfig = {
     return [
       {
         source: "/docs",
-        destination: `${DOCS_URL}/`,
+        destination: `${DOCS_URL}/docs`,
       },
       {
         source: "/docs/:path+",
-        destination: `${DOCS_URL}/:path+`,
+        destination: `${DOCS_URL}/docs/:path+`,
       },
     ];
   },
